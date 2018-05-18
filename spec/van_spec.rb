1806e8c5a8e7b23325df3c7c58262385e1410995
anguspettifer <angus.pettifer@gmail.com>
2018-05-18 17:46:56 +0100

    task complete (ish)

---
+++ spec/van_spec.rb
@@ -0,0 +1,10 @@
+# require 'van'
+#
+# describe Van do
+#   it "van takes broken bikes from docking station" do
+#     broken_bike = double(working?: false)
+#     broken_bike_array = [broken_bike]
+#     docking_station = double(pick_up_bikes: broken_bike_array)
+#     expect(subject.pick_up_bikes)
+#   end
+# end
